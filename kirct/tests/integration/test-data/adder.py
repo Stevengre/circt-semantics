1d34609cae17eaafd4db558803bf8502ceb95979
Stevengre <229474673@qq.com>
2023-11-29 18:57:09 +0800

    determine the structure of the python header, and change the source structure for consistent import of kirct and kimulator

---
+++ kirct/tests/integration/test-data/adder.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from kirct.kimulator.model import KimulatorModel, KimulatorContext, Signal
+
+fpath = Path(__file__).parent.joinpath('adder.mlir')
+kimulator_context: KimulatorContext = KimulatorContext()
+kimulator_context.trace_ever_on(True)
+kimulator_context.init_pattern(fpath)
+
+kimulator_context.signals = {
+    'clock': Signal(name='clock',
+                    abbrev=kimulator_context.gen_abbrev(),
+                    mlir_gen_name='clock',
+                    is_input=True,
+                    signal_type='input',
+                    signal_value=0),
+    'reset': Signal(name='reset',
+                    abbrev=kimulator_context.gen_abbrev(),
+                    mlir_gen_name='reset',
+                    is_input=True,
+                    signal_type='input',
+                    signal_value=0),
+    'io_a': Signal(name='io_a',
+                   abbrev=kimulator_context.gen_abbrev(),
+                   mlir_gen_name='io_a',
+                   is_input=True,
+                   signal_type='input',
+                   signal_value=0),
+    'io_b': Signal(name='io_b',
+                   abbrev=kimulator_context.gen_abbrev(),
+                   mlir_gen_name='io_b',
+                   is_input=True,
+                   signal_type='input',
+                   signal_value=0),
+    'io_out': Signal(name='io_out',
+                     abbrev=kimulator_context.gen_abbrev(),
+                     mlir_gen_name='io_out',
+                     is_input=False,
+                     signal_type='output',
+                     signal_value=0),
+}
+
+adder_model: KimulatorModel = KimulatorModel(module_name='Adder',
+                                             source_file=str(fpath),
+                                             signals={},
+                                             context=kimulator_context)
+
+for signal in kimulator_context.signals.values():
+    adder_model.signals[signal.name] = signal
--- kirct/tests/unit/test-data/adder.generic.mlir
+++ kirct/tests/unit/test-data/adder.generic.mlir
@@ -6,5 +6,5 @@
   ^bb0(%arg0: i1, %arg1: i1, %arg2: i8, %arg3: i8):
     %0 = "comb.add"(%arg2, %arg3) {sv.namehint = "_io_out_T_1"} : (i8, i8) -> i8
     "hw.output"(%0) : (i8) -> ()
-  }) {argLocs = [#loc, #loc1, #loc2, #loc2], argNames = ["clock", "reset", "io_a", "io_b"], comment = "", function_type = (i1, i1, i8, i8) -> i8, parameters = [], resultLocs = [#loc2], resultNames = ["io_out"], sym_name = "Adder"} : () -> ()
+  }) {argLocs = [], argNames = ["clock", "reset", "io_a", "io_b"], comment = "", function_type = (i1, i1, i8, i8) -> i8, parameters = [], resultLocs = [], resultNames = ["io_out"], sym_name = "Adder"} : () -> ()
 }) : () -> ()
