48408b398e40e84092b4e51f75d176c178f7d802
nn020701 <32233403+nn020701@users.noreply.github.com>
2024-09-14 16:02:49 +0800

    feature: add makefile and kbuild. WIP: workflows for github

--- src/kproj/__main__.py
+++ src/kproj/__main__.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from kproj main function!")
+def main() -> None:
+    print('Hello from kproj main function!')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
