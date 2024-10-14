e17d2905933df99618b6a197ce31e9c4eb3e0f5e
Stevengre <229474673@qq.com>
2024-10-15 00:32:05 +0800

    kompile commands

--- save_kompile.sh
+++ save_kompile.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+export K_OPTS=-Xmx128G
 
 # 运行第二个 kompile 命令并将其放入后台运行
+start_time=$(date +%s)
 kompile ./src/kcirct/kdist/circt_semantics/main.k --gen-bison-parser --bison-stack-max-depth 1000000000 -O3 -ccopt -g -o ~/data/$1-opt-kompiled &
 pid2=$!
-echo "Optimized kompiling started with PID: $pid2" &
+echo "Optimized kompiling started with PID: $pid2 " &
 
 # 运行第一个 kompile 命令并将其放入后台运行
 kompile ./src/kcirct/kdist/circt_semantics/main.k --gen-bison-parser --bison-stack-max-depth 1000000000 -o ~/data/$1-kompiled &
 pid1=$!
-echo "Kompiling started with PID: $pid1" &
+echo "Kompiling started with PID: $pid1" 
 
 # 等待第一个 kompile 命令完成并输出消息
 wait $pid1
+end_time=$(date +%s)
+echo "Time taken for kompiling: $((end_time - start_time)) seconds"
 echo "Kompiling done"
+
 
 # 等待第二个 kompile 命令完成并输出消息
 wait $pid2
+end_time=$(date +%s)
+echo "Time taken for optimized kompiling: $((end_time - start_time)) seconds"
 echo "Optimized kompiling done"
