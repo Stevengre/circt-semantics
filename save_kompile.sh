97c251c662843464fe4fcd721674cbbba8b7328c
Stevengre <229474673@qq.com>
2024-10-15 20:41:57 +0800

    getWidth SeqClockType

--- save_kompile.sh
+++ save_kompile.sh
@@ -2,12 +2,14 @@
 
 
 # 运行第二个 kompile 命令并将其放入后台运行
-kompile ./src/kcirct/kdist/circt_semantics/main.k --gen-bison-parser --bison-stack-max-depth 1000000000 -O3 -ccopt -g -o ~/data/$1-opt-kompiled &
+kompile ./src/kcirct/kdist/circt_semantics/main.k -ccopt -g -o ~/data/$1-opt-kompiled &
+# kompile ./src/kcirct/kdist/circt_semantics/main.k --gen-bison-parser --bison-stack-max-depth 1000000000 -O3 -ccopt -g -o ~/data/$1-opt-kompiled &
 pid2=$!
 echo "Optimized kompiling started with PID: $pid2" &
 
 # 运行第一个 kompile 命令并将其放入后台运行
-kompile ./src/kcirct/kdist/circt_semantics/main.k --gen-bison-parser --bison-stack-max-depth 1000000000 -o ~/data/$1-kompiled &
+kompile ./src/kcirct/kdist/circt_semantics/main.k -o ~/data/$1-kompiled &
+# kompile ./src/kcirct/kdist/circt_semantics/main.k --gen-bison-parser --bison-stack-max-depth 1000000000 -o ~/data/$1-kompiled &
 pid1=$!
 echo "Kompiling started with PID: $pid1" &
 
