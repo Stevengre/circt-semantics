754acf811b35397b60d1c59d1e31b90bc16203d0
nn020701 <723221403@qq.com>
2024-10-18 18:21:37 +0800

    test_semantics

--- src/tests/integration/test_semantics.py
+++ src/tests/integration/test_semantics.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 from kcirct.api import KCIRCT
 
+from ..resources import DATA_PATH
+
 NOW_PATH = Path(__file__).parent
 TEST_PATH = NOW_PATH.parent.parent / 'kcirct' / 'kdist' / 'circt_semantics'
 
 def test_syntax() -> None:
-
-    print(TEST_PATH)
     kcirct = KCIRCT()
     kcirct.run(
         [
@@ -37,5 +37,34 @@ def test_syntax() -> None:
             'kore',
             '--output-file',
             str(TEST_PATH / 'test-syntax.kore'),
+        ]
+    )
+
+def test_syntax_no_drop() -> None:
+    kcirct = KCIRCT()
+    kcirct.run(
+        [
+            'kompile',
+            str(TEST_PATH / 'test-syntax.k'),
+            '--gen-bison-parser',
+            '--bison-stack-max-depth',
+            '1000000000',
+            '-o',
+            str(TEST_PATH / 'test-syntax-kompiled/'),
+        ]
+    )
+
+    kcirct.run(
+        [
+            'krun',
+            str(DATA_PATH/ 'rocket-small-nodrop' / 'rocket-small-master.generic.mlir'),
+            '--definition',
+            str(TEST_PATH / 'test-syntax-kompiled'),
+            '--depth',
+            '0',
+            '-o',
+            'kore',
+            '--output-file',
+            str(TEST_PATH / 'test-syntax-no-drop.kore'),
         ]
     )
