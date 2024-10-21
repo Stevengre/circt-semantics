b3239d1e38c1a2a2f4dbe97864c9bb57f401df6b
nn020701 <723221403@qq.com>
2024-10-21 14:48:12 +0800

    change api, use parametrize

--- src/tests/integration/test_semantics.py
+++ src/tests/integration/test_semantics.py
@@ -3,6 +3,7 @@ Validate the semantics of the K-CIRCT.
 """
 
 from pathlib import Path
+import pytest
 
 from kcirct.api import KCIRCT
 
@@ -11,60 +12,16 @@ from ..resources import DATA_PATH
 NOW_PATH = Path(__file__).parent
 TEST_PATH = NOW_PATH.parent.parent / 'kcirct' / 'kdist' / 'circt_semantics'
 
-def test_syntax() -> None:
+@pytest.mark.parametrize(
+    'test_k_file, test_mlir', [
+    (TEST_PATH / 'test-syntax.k',TEST_PATH / 'rocket-small-drop.mlir'),
+    (TEST_PATH / 'test-syntax.k',DATA_PATH/ 'rocket-small-nodrop' / 'rocket-small-master.generic.mlir'),
+    ],
+    ids=['drop','no-drop'],
+)
+def test_syntax(test_k_file: Path, test_mlir: Path) -> None:
     kcirct = KCIRCT()
-    kcirct.run(
-        [
-            'kompile',
-            str(TEST_PATH / 'test-syntax.k'),
-            '--gen-bison-parser',
-            '--bison-stack-max-depth',
-            '1000000000',
-            '-o',
-            str(TEST_PATH / 'test-syntax-kompiled/'),
-        ]
-    )
-
-    kcirct.run(
-        [
-            'krun',
-            str(TEST_PATH / 'rocket-small-drop.mlir'),
-            '--definition',
-            str(TEST_PATH / 'test-syntax-kompiled'),
-            '--depth',
-            '0',
-            '-o',
-            'kore',
-            '--output-file',
-            str(TEST_PATH / 'test-syntax.kore'),
-        ]
-    )
-
-def test_syntax_no_drop() -> None:
-    kcirct = KCIRCT()
-    kcirct.run(
-        [
-            'kompile',
-            str(TEST_PATH / 'test-syntax.k'),
-            '--gen-bison-parser',
-            '--bison-stack-max-depth',
-            '1000000000',
-            '-o',
-            str(TEST_PATH / 'test-syntax-kompiled/'),
-        ]
-    )
-
-    kcirct.run(
-        [
-            'krun',
-            str(DATA_PATH/ 'rocket-small-nodrop' / 'rocket-small-master.generic.mlir'),
-            '--definition',
-            str(TEST_PATH / 'test-syntax-kompiled'),
-            '--depth',
-            '0',
-            '-o',
-            'kore',
-            '--output-file',
-            str(TEST_PATH / 'test-syntax-no-drop.kore'),
-        ]
-    )
+    kcirct.kompile_manual(test_k_file,TEST_PATH / 'test-syntax-kompiled/',
+                          ['--gen-bison-parser','--bison-stack-max-depth','1000000000'])
+    kcirct.set_definition_dir(TEST_PATH / 'test-syntax-kompiled')
+    kcirct.krun_manual(test_mlir,['--depth','0','-o','kore','--output-file',str(TEST_PATH / 'test-syntax.kore')])
