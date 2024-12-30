6440b0cb93606dc10786ef50654d176d6e2bad5f
nn020701 <723221403@qq.com>
2024-12-30 17:15:12 +0800

    cleanProcess

--- src/tests/resources/operation/hw/aggregate_constant2/obj_dir/aggregate_constant2__main.d
+++
@@ -1,6 +0,0 @@
-aggregate_constant2__main.o: ../aggregate_constant2__main.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h
