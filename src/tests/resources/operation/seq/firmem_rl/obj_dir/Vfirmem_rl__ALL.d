eab46b6679afbba1d65f5dabd42b0633cbe763f7
nn020701 <723221403@qq.com>
2025-05-17 17:08:41 +0800

    最后测试CI对Operation测试是否完成

--- src/tests/resources/operation/seq/firmem_rl/obj_dir/Vfirmem_rl__ALL.d
+++
@@ -1,7 +0,0 @@
-Vfirmem_rl__ALL.o: Vfirmem_rl__ALL.cpp Vfirmem_rl.cpp Vfirmem_rl.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vfirmem_rl__Syms.h \
- Vfirmem_rl__Trace.cpp /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vfirmem_rl__Slow.cpp \
- Vfirmem_rl__Syms.cpp Vfirmem_rl__Trace__Slow.cpp
--- src/tests/resources/operation/seq/firmem_rw/obj_dir/Vfirmem_rw__ALL.d
+++
@@ -1,7 +0,0 @@
-Vfirmem_rw__ALL.o: Vfirmem_rw__ALL.cpp Vfirmem_rw.cpp Vfirmem_rw.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vfirmem_rw__Syms.h \
- Vfirmem_rw__Trace.cpp /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vfirmem_rw__Slow.cpp \
- Vfirmem_rw__Syms.cpp Vfirmem_rw__Trace__Slow.cpp
